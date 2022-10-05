d5a00302242e549a571276bdc9b847ba96b69432
Àlex Grabulosa Puertas <alexgrabulosapuertas9@gmail.com>
2022-10-05 20:32:10 +0200

    Feat: Implement time to lose

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const objective = document.getElementById('objective');
 objective.addEventListener('click', upScore);
 const score = document.getElementById('score');
+const time = document.getElementById('time');
 
 const HEIGHT_CONTAINER = 60;
 const WIDTH_CONTAINER = 60;
 
-const WIDTH_PLAYER = 2;
 const HEIGHT_PLAYER = 2;
+const WIDTH_PLAYER = 2;
 
 const MAX_SCORE = 60;
+
+const LIMIT_TIME = 0;
+
+const intervalDownTime = setInterval(downTime, 1000);
+
+function downTime() {
+    const currentTime = Number(time.innerText) - 1;
+    time.innerText = currentTime;
+
+    if (currentTime <= LIMIT_TIME) {
+        gameOver();
+        return;
+    }
+}
+
+function gameOver() {
+    clearInterval(intervalDownTime);
+    alert('GAME OVER! Try again!');
+    location.reload();
+}
 
 function move() {
     objective.style.marginLeft = Math.random() * (WIDTH_CONTAINER - WIDTH_PLAYER) + 'vw';
@@ -28,6 +49,7 @@ function upScore() {
 }
 
 function win() {
+    clearInterval(intervalDownTime);
     alert('CONGRATULATIONS! You\'ve won!');
     location.reload();
 }
